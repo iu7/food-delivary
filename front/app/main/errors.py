46f69e7ab8c957f844782c748d0b28a5a76a71bc
bakit <bakit@debian>
2015-05-27 18:57:23 +0300

    nginx configured

--- front/app/main/errors.py
+++ front/app/main/errors.py
@@ -1,5 +1,14 @@
 from flask import render_template
 from . import main
+from flask import abort
+
+@main.app_errorhandler(Exception)
+def handle_invalid_usage(error):
+	return render_template('500.html'), 500	
+	#response = jsonify(error.to_dict())
+	#response.status_code = error.status_code
+	#return response
+
 
 @main.app_errorhandler(404)
 def page_not_found(e):
@@ -9,3 +18,4 @@ def page_not_found(e):
 @main.app_errorhandler(500)
 def internal_server_error(e):
 	return render_template('500.html'), 500
+
